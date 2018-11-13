7e1e6680da277c7007bbeb53cf01cbf630ae0f14
dexmo007 <henrik.drefs@outlook.de>
2018-11-13 21:28:30 +0100

    expose some maven properties in swagger

--- src/main/java/com/dexmohq/gotswag/GotSwagApplication.java
+++ src/main/java/com/dexmohq/gotswag/GotSwagApplication.java
@@ -1,8 +1,10 @@
 package com.dexmohq.gotswag;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,23 +14,30 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.io.IOException;
 import java.util.Collections;
+import java.util.Properties;
 
 @SpringBootApplication
 @EnableSwagger2
 public class GotSwagApplication {
 
+    @Value("classpath:pom.properties")
+    private Resource pomProperties;
+
     @Bean
-    public Docket docket() {
+    public Docket docket() throws IOException {
+        final Properties properties = new Properties();
+        properties.load(pomProperties.getInputStream());
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.dexmohq"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(new ApiInfo(
-                        "webhooker", "Hooking the web", "1.0-SNAPSHOT", null,
+                        "webhooker", "Hooking the web", properties.getProperty("version"), null,
                         new Contact("Henrik Drefs", "https://github.com/dexmo007", null),
-                        "MIT", "https://opensource.org/licenses/MIT", Collections.emptyList()
+                        properties.getProperty("license.name"), properties.getProperty("license.url"), Collections.emptyList()
                 ))
                 .consumes(Collections.singleton(MediaType.APPLICATION_JSON_VALUE))
                 .produces(Collections.singleton(MediaType.APPLICATION_JSON_VALUE))
